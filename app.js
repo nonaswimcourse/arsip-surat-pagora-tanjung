f026560dc4573a1501ab2c88f9fe426bbb8dc8a2
nona swimming course <keishaayundiainara27@gmail.com>
2026-06-20 00:13:14 +0700

    Add login, checkSession, and logout functions

--- app.js
+++ app.js
@@ -18,3 +18,51 @@ async function testSupabase() {
 }
 
 testSupabase();
+async function login() {
+
+  const email =
+    document.getElementById("email").value;
+
+  const password =
+    document.getElementById("password").value;
+
+  const { data, error } =
+    await supabaseClient.auth.signInWithPassword({
+      email,
+      password
+    });
+
+  if(error){
+    alert(error.message);
+    return;
+  }
+
+  document.getElementById("loginPage")
+    .style.display = "none";
+
+  document.getElementById("app")
+    .style.display = "block";
+}
+async function checkSession(){
+
+  const {
+    data: { session }
+  } = await supabaseClient.auth.getSession();
+
+  if(session){
+
+    document.getElementById("loginPage")
+      .style.display = "none";
+
+    document.getElementById("app")
+      .style.display = "block";
+  }
+}
+
+checkSession();
+async function logout(){
+
+  await supabaseClient.auth.signOut();
+
+  location.reload();
+}
