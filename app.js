99fae50c709110b2eb9346f4a82c1f304be3d107
nona swimming course <keishaayundiainara27@gmail.com>
2026-06-19 23:54:30 +0700

    Add testConnection function to check Supabase connection

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const SUPABASE_ANON_KEY='sb_publishable_1Vq-L40vJKQMT6CfuORWMQ_Iom2dRSj';
 const supabaseClient = supabase.createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
 
 console.log('SIPAS KKG PJOK Ready');
+async function testConnection() {
+  const { data, error } = await supabaseClient
+    .from('pengaturan')
+    .select('*');
+
+  console.log('DATA:', data);
+  console.log('ERROR:', error);
+}
+
+testConnection();
