def5c5f41a083204cdb8ef9f3f71c6c5635cf899
nona swimming course <keishaayundiainara27@gmail.com>
2026-06-27 09:29:11 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'pagora-tanjung-word-front-layer-v1';
+const CACHE_NAME = 'pagora-tanjung-word-signature-close-v1';
 const APP_SHELL = [
   './',
   './index.html',
-  './assets/style.css?v=20260627-word-front-layer-1',
-  './assets/app.js?v=20260627-word-front-layer-1',
+  './assets/style.css?v=20260626-word-signature-close-1',
+  './assets/app.js?v=20260626-word-signature-close-1',
   './assets/stempel-kkg-pjok.png',
   './logo.png',
   './assets/logo.png',
